b89ee49d88aec014b0619e6edbcc54ee40d391b6
Thomkok <thomkok13@hotmail.com>
2020-01-13 18:03:44 +0100

    nieuwe routes toegevoegd

--- routes/web.js
+++ routes/web.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const homeController = require("../controllers/homeController");
 const conferencesController = require("../controllers/conferenceController");
+const locationsController = require("../controllers/locationController");
+const roomsController = require("../controllers/roomController");
 
 const router = express.Router();
 
+// Home
 router.get('/', homeController.getHome);
+
+// Conference
 router.get('/conference', conferencesController.getAllconferences);
 router.get('/conference/show/:id', conferencesController.getConference);
 router.get('/conference/edit/:id', conferencesController.getEditConference);
 router.post('/conference/update/:id', conferencesController.postUpdateConference);
-router.get('/conference/destroy/:id', conferencesController.getDestroyConference);
-router.get('/conference/add', conferencesController.getAddConference);
-router.post('/conference/add', conferencesController.postStoreConference);
+router.post('/conference/destroy/:id', conferencesController.postDestroyConference);
+router.get('/conference/create', conferencesController.getAddConference);
+router.post('/conference/store', conferencesController.postStoreConference);
+
+// Location
+router.get('/location', locationsController.getAllLocations);
+
+// Room
+router.get('/room', roomsController.getAllRooms);
 
 module.exports = router;
